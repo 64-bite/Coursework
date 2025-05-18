5a63b35bb42866f48f1eed5c48ffa3cafd9da97e
64-bite <matvienkonazar7@gmail.com>
2025-05-18 22:09:12 +0300

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,28 +1,49 @@
 #include "utils.h"
 
-void Utils::setBarrierRows(ShipsPlacement& placement) {
-    for (int i = 0; i < board_size; i++) {
-        if (placement.white_rows[i] == ZERO && placement.black_rows[i] == ZERO) {
-            for (int j = 0; j < board_size; j++)
-                placement.board.board[i][j] = BARRIER;
-        }
-    }
+void setBarrierRows() {
+	for(int i = 0; i < board_size; i++) {
+		for(int j = 0; j < board_size; j++) {
+			if(white_rows[i] == ZERO && black_rows[i] == ZERO) {
+			board[j][i] = BARRIER;
+			} else if(white_rows[i] == EIGHT) {
+				for(int k = 1; k < EIGHT; k++){
+					board[k][i] = BARRIER;
+			}
+			} else if(white_rows[i] == SEVEN) { 
+				for(int k = 2; k < SEVEN; k++){
+					board[k][i] = BARRIER;
+				}
+			} else if(white_rows[i] == SIX) {
+				for(int k = 3; k < EIGHT; k++) {
+				board[k][i] = BARRIER;
+				}
+			}	
+		}
+	}
 }
-
-void Utils::setBarrierCols(ShipsPlacement& placement) {
-    for (int i = 0; i < board_size; i++) {
-        if (placement.white_cols[i] == ZERO && placement.black_cols[i] == ZERO) {
-            for (int j = 0; j < board_size; j++)
-                placement.board.board[j][i] = BARRIER;
-        }
-    }
+void setBarrierCols() {
+	if(WhiteCols[i] == ZERO && BlackCols[i] == ZERO) {
+		board[i][j] = BARRIER;
+	} else if(WhiteCols[i] == EIGHT) {
+		for(int k = 1; k < EIGHT; k++){
+			board[i][k] = BARRIER;
+		}
+	} else if(WhiteCols[i] == SEVEN) { 
+		for(int k = 2; k < SEVEN; k++){
+			board[i][k] = BARRIER;
+		}
+	} else if(WhiteCols[i] == SIX) {
+		for(int k = 3; k < EIGHT; k++) {
+			board[i][k] = BARRIER;
+		}
+	}
 }
-
-void Utils::removeBarrier(ShipsPlacement& placement) {
-    for (int i = 0; i < board_size; i++) {
-        for (int j = 0; j < board_size; j++) {
-            if (placement.board.board[i][j] == BARRIER)
-                placement.board.board[i][j] = EMPTY;
-        }
-    }
+void remove_barrier(){ 
+	for(int i = 0; i < board_size; i++) {
+		for(int j = 0; j < board_size; j++) {
+			if(board[i][j] == BARRIER) {
+				board[i][j] = EMPTY;
+			}
+		}
+	}
 }
