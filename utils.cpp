96502f28a9acdd21c7f3a5f2cfd0a3b79a666c4e
64-bite <matvienkonazar7@gmail.com>
2025-05-22 10:22:16 +0300

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,49 +1,59 @@
 #include "utils.h"
 
-void setBarrierRows() {
-	for(int i = 0; i < board_size; i++) {
-		for(int j = 0; j < board_size; j++) {
-			if(white_rows[i] == ZERO && black_rows[i] == ZERO) {
-			board[j][i] = BARRIER;
-			} else if(white_rows[i] == EIGHT) {
-				for(int k = 1; k < EIGHT; k++){
-					board[k][i] = BARRIER;
-			}
-			} else if(white_rows[i] == SEVEN) { 
-				for(int k = 2; k < SEVEN; k++){
-					board[k][i] = BARRIER;
-				}
-			} else if(white_rows[i] == SIX) {
-				for(int k = 3; k < EIGHT; k++) {
-				board[k][i] = BARRIER;
-				}
-			}	
-		}
-	}
+void Utils::setBarrierRows(ShipsPlacement& placement) {
+    for (int i = 0; i < board_size; i++) {
+        for (int j = 0; j < board_size; j++) {
+            if (placement.white_rows[i] == ZERO && placement.black_rows[i] == ZERO) {
+                b.board[i][j] = BARRIER;
+            }
+        }
+
+        if (placement.white_rows[i] == EIGHT) {
+            for (int k = 1; k < EIGHT; k++) {
+                b.board[i][k] = BARRIER;
+            }
+        } else if (placement.white_rows[i] == SEVEN) {
+            for (int k = 2; k < SEVEN; k++) {
+                b.board[i][k] = BARRIER;
+            }
+        } else if (placement.white_rows[i] == SIX) {
+            for (int k = 3; k < EIGHT; k++) {
+                b.board[i][k] = BARRIER;
+            }
+        }
+    }
 }
-void setBarrierCols() {
-	if(WhiteCols[i] == ZERO && BlackCols[i] == ZERO) {
-		board[i][j] = BARRIER;
-	} else if(WhiteCols[i] == EIGHT) {
-		for(int k = 1; k < EIGHT; k++){
-			board[i][k] = BARRIER;
-		}
-	} else if(WhiteCols[i] == SEVEN) { 
-		for(int k = 2; k < SEVEN; k++){
-			board[i][k] = BARRIER;
-		}
-	} else if(WhiteCols[i] == SIX) {
-		for(int k = 3; k < EIGHT; k++) {
-			board[i][k] = BARRIER;
-		}
-	}
+
+void Utils::setBarrierCols(ShipsPlacement& placement) {
+    for (int i = 0; i < board_size; i++) {
+        for (int j = 0; j < board_size; j++) {
+            if (placement.white_cols[i] == ZERO && placement.black_cols[i] == ZERO) {
+                b.board[j][i] = BARRIER;
+            }
+        }
+
+        if (placement.white_cols[i] == EIGHT) {
+            for (int k = 1; k < EIGHT; k++) {
+                b.board[k][i] = BARRIER;
+            }
+        } else if (placement.white_cols[i] == SEVEN) {
+            for (int k = 2; k < SEVEN; k++) {
+                b.board[k][i] = BARRIER;
+            }
+        } else if (placement.white_cols[i] == SIX) {
+            for (int k = 3; k < EIGHT; k++) {
+                b.board[k][i] = BARRIER;
+            }
+        }
+    }
 }
-void remove_barrier(){ 
-	for(int i = 0; i < board_size; i++) {
-		for(int j = 0; j < board_size; j++) {
-			if(board[i][j] == BARRIER) {
-				board[i][j] = EMPTY;
-			}
-		}
-	}
+
+void Utils::removeBarrier() {
+    for (int i = 0; i < board_size; i++) {
+        for (int j = 0; j < board_size; j++) {
+            if (b.board[i][j] == BARRIER) {
+                b.board[i][j] = EMPTY;
+            }
+        }
+    }
 }
