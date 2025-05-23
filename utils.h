ae2542bfb9fc1021419fffbf2355e0dddf1c0673
64-bite <matvienkonazar7@gmail.com>
2025-05-23 10:58:39 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -2,17 +2,24 @@
 #define UTILS_H
 
 #include "board.h"
-#include "ships_placement.h"
-
-enum BarrierNum { SIX = 6, SEVEN = 7, EIGHT = 8, ZERO = 0 };
+#include "shipsPlacement.h"
+ /*----------------------------------------------------------------<Header>-
+ Name: utils.h
+ Title: Японський кросворд з човнами
+ Group: ТВ-43
+ Student: Матвієнко
+ Written: 2025-05-22
+ Revised: 2025-05-23
+ Description: Хеш функція по роботі з барьєрами
+ ------------------------------------------------------------------</Header>-*/
+enum BarrierNum { ZERO = 0, ONE = 1, TWO = 2, THREE = 3, SIX = 6, SEVEN = 7, EIGHT = 8 };//Умови для барє'рів
 
 class Utils {
+    Board board;
 public:
-    Board b;
     void setBarrierRows(ShipsPlacement& placement);
     void setBarrierCols(ShipsPlacement& placement);
     void removeBarrier();
 };
 
 #endif
-
