7de0c9a580bedde0ea34322c07c9cefc9c73f3a9
64-bite <matvienkonazar7@gmail.com>
2025-05-23 10:55:34 +0300

    Update puzzle_solver.h

--- puzzle_solver.h
+++ puzzle_solver.h
@@ -1,18 +1,27 @@
 #ifndef PUZZLE_SOLVER_H
 #define PUZZLE_SOLVER_H
-
-#include "ships_placement.h"
-#include "utils.h"
 #include <string>
+#include "shipsPlacement.h"
+#include "utils.h"
+#include "board.h"
+ /*----------------------------------------------------------------<Header>-
+ Name: puzzleSolver.h
+ Title: Японський кросворд з човнами
+ Group: ТВ-43
+ Student: Матвієнко
+ Written: 2025-05-22
+ Revised: 2025-05-23
+ Description: Хеш функція оголошує функції по вирішенню головоломки і виводу виконаної головоломки в файл
+ ------------------------------------------------------------------</Header>-*/
 class PuzzleSolver {
+	ShipsPlacement placement;
+    Utils utils;
+	Board board;
 public:
     PuzzleSolver(const ShipsPlacement& placement);
     bool solve(int shipIndex = 0);
     bool repairBoard();
     void printConsoleFile(const std::string& filename);
-private:
-    ShipsPlacement placement;
-    Utils utils;
 };
 
 #endif
